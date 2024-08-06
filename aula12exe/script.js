c811a64fa4d3763cd6e336a54653a7fa4ba798c4
Gabriel Abade <161393183+gabrielvitorabade@users.noreply.github.com>
2024-08-06 20:44:37 -0300

    adição de comprimento

--- aula12exe/script.js
+++ aula12exe/script.js
@@ -1,6 +1,7 @@
 function carregar(){
     msg = window.document.getElementById(`msg`)
     img = window.document.getElementById('imagem')
+    cmp = window.document.getElementById('comp')
     var data = new Date
     var hora = data.getHours()
     var minutos = data.getMinutes()
@@ -8,11 +9,14 @@ function carregar(){
     if (hora >= 0 && hora <= 12 ){
         img.src = "imagens/manha.png"
         document.body.style.background = '#ecd2a2'
+        cmp.innerHTML = `Bom dia!`
     } else if(hora <= 17){
         img.src = "imagens/tarde.png"
         document.body.style.background = '#be7b6a'
+        cmp.innerHTML = `Boa tarde!`
     }else{
         img.src= "imagens/noite.png"
         document.body.style.background = "#001954"
+        cmp.innerHTML = 'Boa noite!'
     }
 }
